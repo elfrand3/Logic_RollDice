cb9a0269a6f357f27a9a1f18c5bf0f6e7bdcf6ad
el_codee <elfrand3@gmail.com>
2023-02-06 13:46:09 +0700

    Spin dice atau putar judi

---
+++ app/src/main/java/com/elcodee/roll_dice/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.elcodee.roll_dice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.constraintlayout.motion.utils.StopLogic
+import com.elcodee.roll_dice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.btnRoll.setOnClickListener {
+            val dice = rollDice(6)
+            val spinDice = dice.imageDice()
+            val listDice = when(spinDice){
+                1 -> R.drawable.dice_one
+                2 -> R.drawable.dice_two
+                3 -> R.drawable.dice_three
+                4 -> R.drawable.dice_four
+                5 -> R.drawable.dice_five
+                else -> R.drawable.dice_six
+            }
+            if (spinDice == 6){
+                binding.ivDice.setImageResource(listDice)
+                binding.btnRoll.isEnabled = false
+            }else{
+                binding.ivDice.setImageResource(listDice)
+            }
+        }
+    }
+}
+class rollDice(private val runDice: Int){
+    fun imageDice(): Int{
+        return (1 .. runDice).random()
+    }
+}
