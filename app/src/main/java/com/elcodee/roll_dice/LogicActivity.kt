a7424b82a4950773c5ba2e7dd1f5c5834fde95b6
el_codee <elfrand3@gmail.com>
2023-02-07 14:13:52 +0700

    update

---
+++ app/src/main/java/com/elcodee/roll_dice/LogicActivity.kt
@@ -0,0 +1,11 @@
+package com.elcodee.roll_dice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogicActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logic)
+    }
+}
